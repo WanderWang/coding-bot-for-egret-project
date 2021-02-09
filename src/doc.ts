0abfd91111999081edc7714b6d4e998aa16315b3
wangze <wangze@egret.com>
2021-02-09 11:58:00 +0800

    update

--- src/doc.ts
+++ src/doc.ts
@@ -31,6 +31,10 @@ async function run(projectPath: string) {
 
 
     function check(source: SourceFile) {
+        source.getNamespaces().forEach(n => {
+            n.getClasses().forEach(checkClass);
+            n.getFunctions().forEach(executeJsDocs);
+        })
         source.getClasses().forEach(checkClass);
         source.getFunctions().forEach(executeJsDocs);
     }
@@ -38,13 +42,13 @@ async function run(projectPath: string) {
     const project = new Project({
         tsConfigFilePath: path.join(projectPath, 'tsconfig.json')
     });
-    const sourceFiles = project.getSourceFiles();
+    const sourceFiles = project.getSourceFiles().filter(s => !s.isDeclarationFile())
     sourceFiles.forEach(check);
-
+    console.log(222)
     project.saveSync();
 
 }
 
-const projectPath = '../egret/packages/tween'
+const projectPath = '../egret/packages/eui'
 
 run(projectPath);
