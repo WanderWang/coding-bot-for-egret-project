d004c23180f6504b2829dc2c48a39f59b779b000
wangze <wangze@egret.com>
2021-02-09 11:53:33 +0800

    update

--- src/convert-to-es6-module.ts
+++ src/convert-to-es6-module.ts
@@ -4,9 +4,10 @@ import { NamespaceDeclarationKind, Project, SourceFile } from "ts-morph";
 async function run(projectPath: string) {
 
     function execute(s: SourceFile) {
-
+        console.log(1)
         const allNamespaces = s.getNamespaces();
         for (let namespace of allNamespaces) {
+            console.log(2)
             const statments = namespace.getStatementsWithComments();
             for (let statment of statments) {
                 s.addStatements(statment.getText());
@@ -16,12 +17,13 @@ async function run(projectPath: string) {
 
         }
         console.log(s.print())
+        process.exit();
     }
 
     const project = new Project({
         tsConfigFilePath: path.join(projectPath, 'tsconfig.json')
     });
-    const sourceFiles = project.getSourceFiles();
+    const sourceFiles = project.getSourceFiles().filter(s => !s.isDeclarationFile())
 
 
     sourceFiles.forEach(execute);
@@ -30,6 +32,6 @@ async function run(projectPath: string) {
 
 }
 
-const projectPath = './tests/convert-to-es6-module'
+const projectPath = '../egret/packages/eui'
 
 run(projectPath);
